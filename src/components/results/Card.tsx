744f5f1b16e64a0410559ccccb9a826eb37cedd3
Arathi Vallipuranathan <avallipuranathan@gmail.com>
2024-09-12 20:12:10 -0400

    test commit

--- src/components/results/Card.tsx
+++ src/components/results/Card.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 
 interface CardProps {
-  itemLabel: string;
+  name: string;
+  image: string;
 }
 
-const Card: React.FC<CardProps> = ({ itemLabel }) => {
+const Card: React.FC<CardProps> = ({ name, image }) => {
   return (
-    <div className="w-48 h-48 flex items-center justify-center bg-gray-200 rounded-2xl cursor-pointer hover:bg-accent transition-all duration-300">
-      <h1 className="text-2xl text-secondary">{itemLabel}</h1>
+    <div className="flex flex-col items-center justify-center">
+      <div
+        className="w-48 h-48 bg-gray-200 rounded-2xl cursor-pointer hover:bg-accent transition-all duration-300"
+        style={{
+          backgroundImage: `url(${image})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+        }}
+      >xxxx</div>
+      <h1 className="text-md text-secondary ">{name}</h1>
     </div>
   );
 };
