daa9a33a11f7ad9c6db02f5d03b69d4af091b6a9
Arathi Vallipuranathan <avallipuranathan@gmail.com>
2024-10-08 12:40:26 -0400

    create nation stats page

--- src/components/Stats/NationStats.tsx
+++ src/components/Stats/NationStats.tsx
@@ -1,22 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import StatsTable from "./StatsTable";
+import NationData from "../../utils/nations.json";
 
 const PositionStats: React.FC = () => {
   const { name } = useParams<{ name: string }>();
   const [players, setPlayers] = useState([]);
 
+  const getCodeByName = (name: string) => {
+    const country = NationData.find((country) => country.name === name);
+    return country ? country.code : null; // Return code or null if not found
+  };
+
+
   useEffect(() => {
-    // Fetch players for the selected position from your API
     const fetchPlayers = async () => {
-      try {
-        const response = await fetch(
-          `http://localhost:8080/api/player/position/${name}`
-        );
-        const data = await response.json();
-        setPlayers(data);
-      } catch (error) {
-        console.error("Error fetching players:", error);
+      if (name) {
+        const nationCode = getCodeByName(name);
+
+        try {
+          const response = await fetch(
+            `http://localhost:8080/api/player/nation/${nationCode?.toLowerCase()}`
+          );
+          const data = await response.json();
+          setPlayers(data);
+        } catch (error) {
+          console.error("Error fetching players:", error);
+        }
+      } else {
+        console.warn("Team name is undefined");
       }
     };
 
@@ -24,9 +36,11 @@ const PositionStats: React.FC = () => {
   }, [name]);
 
   return (
-    <div>
-      <h1 className="text-2xl mt-6 text-secondary">{name} Players</h1>
-      <StatsTable />
+    <div className="w-full flex items-start justify-center">
+      <div className="w-full flex flex-col items-center justify-center mt-4 mb-16">
+        <h1 className="text-3xl mt-6 text-secondary mb-4">{name} Players</h1>
+        <StatsTable players={players} />
+      </div>
     </div>
   );
 };
